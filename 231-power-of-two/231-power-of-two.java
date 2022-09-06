0246872724fdf9467bb0b6b1f062420de81e1e66
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2022-09-06 09:40:02 +0530

    Time: 2 ms (71.20%), Space: 41.5 MB (33.20%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -2,12 +2,7 @@ class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n==0)
             return false;
-        while(n%2==0){
-            n=n/2;
-        }
-        if(n==1)
-            return true;
-        return false;
-        
+        long x=(long) n;
+        return (x & (x-1) )==0;
     }
 }
