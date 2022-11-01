3b11160cdbe98b67f3a2ba6c3810555be72b0474
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2022-11-01 18:19:44 +0530

    Time: 3 ms (30.24%), Space: 54.1 MB (55.13%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,33 +1,60 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+    
+        boolean isColZero=false;
+        boolean isRowZero=false;
         
-        int[][] clone=new int[matrix.length][matrix[0].length];
+        // checking if any element in 0th col is zero
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                clone[i][j]=matrix[i][j];
+            if(matrix[i][0]==0){
+                isColZero=true;
+                break;
             }
         }
         
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
+        // checking if any element in 0th row is zero
+        for(int j=0;j<matrix[0].length;j++){
+            if(matrix[0][j]==0){
+                isRowZero=true;
+                break;
+            }
+        }
+        
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-                    modify(clone,i,j);
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
                 }
             }
-        } 
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                matrix[i][j]=clone[i][j];
+        }
+        
+        for(int i=1;i<matrix.length;i++){
+            if(matrix[i][0]==0){
+                setthatrow(matrix,i);
             }
         }
+        
+        for(int j=1;j<matrix[0].length;j++){
+            if(matrix[0][j]==0){
+                setthatcol(matrix,j);
+            }
+        }
+        
+        if(isRowZero)
+            setthatrow(matrix,0);
+        if(isColZero)
+            setthatcol(matrix,0);
     }
     
-    public void modify(int[][] clone,int i,int j){
-        for(int row=0;row<clone.length;row++){
-            clone[row][j]=0;
+    public void setthatrow(int[][] matrix,int row){
+        for(int col=0;col<matrix[0].length;col++){
+            matrix[row][col]=0;
         }
-        for(int col=0;col<clone[0].length;col++){
-            clone[i][col]=0;
+    }
+    public void setthatcol(int[][] matrix,int col){
+        for(int row=0;row<matrix.length;row++){
+            matrix[row][col]=0;
         }
     }
 }
